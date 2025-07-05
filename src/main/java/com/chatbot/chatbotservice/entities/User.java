f9a7682f9ea5280cd97799c2475671c518ae9781
varuncns <37303602+varuncns@users.noreply.github.com>
2025-07-05 22:21:40 +0530

    postgresSQL + Docker

---
+++ src/main/java/com/chatbot/chatbotservice/entities/User.java
@@ -0,0 +1,21 @@
+package com.chatbot.chatbotservice.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
+
